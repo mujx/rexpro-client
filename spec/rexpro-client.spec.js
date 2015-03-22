77d58b44166bbf83ecd49f79a9ea2f69d3976a13
mujx <siderisk@auth.gr>
2015-03-22 22:34:26 +0200

    Implements sessions, adds tests

--- spec/rexpro-client.spec.js
+++ spec/rexpro-client.spec.js
@@ -31,8 +31,9 @@ describe('Ad-hoc queries', function() {
     .then(function (data) {
       expect(data[0]._properties.name).toBe('npm');
       done();
-    }, function(error) {
-      expect(err).toBe(null);
+    })
+    .catch(function(err) {
+      expect(err).toBeUndefined();
       done();
     })
     .done();
@@ -50,8 +51,9 @@ describe('Ad-hoc queries', function() {
     .then(function(data) {
       expect(data[0]._properties.name).toBe('npm');
       done();
-    }, function(err) {
-      expect(err).toBe(null);
+    })
+    .catch(function(err) {
+      expect(err).toBeUndefined();
       done();
     })
     .done();
@@ -69,7 +71,8 @@ describe('Ad-hoc queries', function() {
     .then(function(data) {
       expect(data).toBeUndefined();
       done();
-    }, function(err) {
+    })
+    .catch(function(err) {
       expect(err).toMatch(/^Error: GraphDoesntExist/);
       done();
     })
@@ -86,7 +89,8 @@ describe('Ad-hoc queries', function() {
     .then(function(data) {
       expect(data).toBeUndefined();
       done();
-    }, function(err) {
+    })
+    .catch(function(err) {
       expect(err).toMatch(/^Error: connect ECONNREFUSED/);
       done();
     })
@@ -111,7 +115,8 @@ describe('Ad-hoc queries', function() {
     .then(function(data) {
       expect(data).toBeUndefined();
       done();
-    }, function(err) {
+    })
+    .catch(function(err) {
       expect(err).toMatch(/^Error: ScriptFailure/);
       done();
     })
@@ -125,13 +130,15 @@ describe('In session queries', function() {
     this.graph = process.env.GRAPH_NAME;
     this.remoteHost = process.env.GRAPH_HOST;
 
+    this.UUID = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+
     this.client = new RexProClient({
       host: this.remoteHost,
       graph: this.graph
     });
 
     this.query = {
-      script: 'g.V.has("name", name).out()',
+      script: 'g.V.has("name", name)',
       bindings: {
         name: 'npm'
       }
@@ -141,93 +148,127 @@ describe('In session queries', function() {
 
   it('should be able to open a session', function(done) {
 
-    var sessionInfo = {
-      name: 'testing'
-    };
+    var _this = this;
 
-    var UUID = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
-
-    this.client.openSession(sessionInfo)
+    this.client.openSession()
     .then(function(uuid) {
-      expect(uuid).toMatch(UUID);
+      expect(uuid).toMatch(_this.UUID);
       done();
-    }, function(err) {
-      expect(err).toBe(null);
+    })
+    .catch(function(err) {
+      expect(err).toBeUndefined();
       done();
     })
     .done();
   });
 
-  xit('should be able to open a session using authentication' , function() {
+  it('should be able to execute queries with authentication' , function(done) {
 
+    // If the authentication isn't turned on, it falls back to an
+    // unauthenticated session.
     var sessionInfo = {
-      name: 'testing',
-      username: 'user',
-      password: 'user1234'
+      username: 'rexster',
+      password: 'rexster'
     };
 
-    this.client.openSession(sessionInfo);
+    var _this = this;
 
-    expect(this.client.session('testing')).not.toBeUndefined();
-
-  });
-
-  xit('should be able to close a session', function() {
-
-    var sessionInfo = {
-      name: 'testing',
-    };
-
-    this.client.openSession(sessionInfo);
-
-    expect(this.client.session('testing')).not.toBeUndefined();
-
-    this.client.closeSession(sessionInfo);
-
-    expect(this.client.session('testing')).toBeUndefined();
-
-  });
-
-  xit('should be albe to execute a query within a session', function() {
-
-    var sessionInfo = {
-      name: 'testing',
-    };
-
-    this.client.openSession(sessionInfo);
-
-    expect(this.client.session('testing')).not.toBeUndefined();
-
-    this.client
-        .session('testing')
-        .execute(this.query, function(err, data) {
-
-      expect(err).toBe(null);
-      expect(data).not.toBeUndefined();
+    this.client.openSession(sessionInfo)
+    .then(function(uuid) {
+      expect(uuid).toMatch(_this.UUID);
+      _this.query.session = uuid;
+      _this.query.console = true;
+      _this.query.script = 'g.V.has("name", name).name';
+      return _this.client.execute(_this.query);
+    })
+    .then(function(data) {
+      expect(data[0]).toBe('npm');
+      return _this.client.closeSession(_this.query);
+    })
+    .then(function(response) {
+      expect(response).toBeTruthy();
+      done();
+    })
+    .catch(function(err) {
+      expect(err).toBeUndefined()
+      done();
     });
+  });
+
+  it('should be able to close a session', function(done) {
+
+    var _this = this;
+
+    var q1 = {
+      script: 'g.V.has("name", name)',
+    };
+
+    this.client.openSession()
+    .then(function(uuid) {
+      expect(uuid).toMatch(_this.UUID);
+      _this.currentUUID = uuid;
+      _this.query.session = uuid;
+      return _this.client.execute(_this.query);
+    })
+    .then(function(data) {
+      expect(data[0]._properties.name).toBe('npm');
+      var q2 = {
+        session: _this.currentUUID
+      };
+      return _this.client.closeSession(q2);
+    })
+    .then(function(response) {
+      expect(response).toBeTruthy();
+      // Should fail because the session no longer exists.
+      q1.session = _this.currentUUID;
+      return _this.client.execute(q1);
+    })
+    .catch(function(err) {
+      expect(err).toMatch(/^Error: SessionDoesntExist/);
+      done();
+    })
+    .done();
 
   });
 
-  xit('should be able to choose different sessions', function() {
+  it('should be albe to execute a query within a session', function(done) {
 
-    var sessionInfo = {
-      name: 'test1'
+    var _this = this;
+
+    var q1 = {
+      script: 'g.V.has("name", name)',
+      bindings: {
+        name: 'npm',
+        another: 'bower'
+      }
     };
 
-    this.client.openSession(sessionInfo);
-
-    expect(this.client.session('test1')).not.toBeUndefined();
-
-    var sessionInfo = {
-      name: 'test2'
+    var q2 = {
+      script: 'g.V.has("name", another)',
     };
 
-    this.client.openSession(sessionInfo);
 
-    expect(this.client.session('test2')).not.toBeUndefined();
-
-    expect(this.client.session('test1')).not.toBeUndefined();
+    this.client.openSession()
+    .then(function(uuid) {
+      expect(uuid).toMatch(_this.UUID);
+      _this.currentUUID = uuid;
+      q1.session = uuid;
+      return _this.client.execute(q1);
+    })
+    .then(function(data) {
+      expect(data[0]._properties.name).toBe('npm');
+      q2.session = _this.currentUUID;
+      return _this.client.execute(q2);
+    })
+    .then(function(data) {
+      expect(data[0]._properties.name).toBe('bower');
+      done();
+    })
+    .catch(function(err) {
+      expect(err).toBeUndefined();
+      done();
+    })
+    .done();
 
   });
-
 });
